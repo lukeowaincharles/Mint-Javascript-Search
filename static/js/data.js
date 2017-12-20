1dd1053e38459fd55545b10fadb45aedc77cc842
Luke Charles <lukecharles@Lukes-Mac-mini.local>
2017-12-20 12:57:02 +0000

    json type file to hold image data

---
+++ static/js/data.js
@@ -0,0 +1,77 @@
+
+var siteData = {
+    "keywords" : [
+        {
+            "name": "Beach",
+            "keywords": "Beach, Sand",
+            "img": "beach.jpg",
+            "alt": "Beach Sea Sand"
+        },
+        {
+            "name": "Coffee",
+            "keywords": "Coffee",
+            "img": "coffee.jpg",
+            "alt": "Coffee"
+        },
+        {
+            "name": "Fantasy",
+            "keywords": "Fantasy",
+            "img": "fantasy.jpg",
+            "alt": "Fantasy Book Girl"
+        },
+        {
+            "name": "Guitarist",
+            "keywords": "Guitaist",
+            "img": "guitarist.jpg",
+            "alt": "Guitarist Amps Guitar"
+        },
+        {
+            "name": "Moose",
+            "keywords": "Moose",
+            "img": "moose.jpg",
+            "alt": "Moose Elk Canada"
+        },
+        {
+            "name": "Neon",
+            "keywords": "Neon",
+            "img": "neon-sign.jpg",
+            "alt": "Neon Sign"
+        },
+        {
+            "name": "Pancake",
+            "keywords": "Pancake",
+            "img": "pancake-stack.jpg",
+            "alt": "Pancake Stack Syrup Blueberries"
+        },
+        {
+            "name": "Puffin",
+            "keywords": "Puffin",
+            "img": "puffin.jpg",
+            "alt": "Puffin Bird"
+        },
+        {
+            "name": "Road",
+            "keywords": "Road",
+            "img": "road-from-above.jpg",
+            "alt": "Road Trees"
+        },
+        {
+            "name": "Skater",
+            "keywords": "Skater",
+            "img": "skater.jpg",
+            "alt": "Skater Skateboard Skatepark"
+        },
+        {
+            "name": "Sunset",
+            "keywords": "Sunset",
+            "img": "sunset.jpg",
+            "alt": "Sunset"
+        },
+        {
+            "name": "Surfer",
+            "keywords": "Surfer",
+            "img": "surfer.jpg",
+            "alt": "Surfer Sun Beach Sea Sand"
+        }
+    ]
+}
